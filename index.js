07a936d8a499e74dd34bacd2723ca909d4dfc98b
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-03-17 23:59:29 -0500

    worked on resonsive styling

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const hamburger = document.getElementsByClassName("hamburger")[0];
+const header = document.getElementsByClassName("header")[0];
 
+hamburger.addEventListener("click", () => {
+  console.log("clicked");
+  header.classList.toggle("active");
+});
